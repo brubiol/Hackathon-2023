9440f7aac367421d8665c284bd7bf6bed93e61c4
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-02-11 13:48:19 -0600

    update login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,14 +6,11 @@ const Login = () => {
             <Nav></Nav>
             <div className="grow-[1] flex justify-center items-center">
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <div className="card-body flex flex-col items-center">
+                    <div className="card-body flex flex-col items-center gap-2">
                         <h2 className="card-title">Welcome Back!</h2>
-                        <div className="card-actions justify-end">
-                            <label className="input-group">
-                                <span>Email</span>
-                                <input type="text" placeholder="info@site.com" className="input input-bordered" />
-                            </label>
-                        </div>
+                        <input type="text" placeholder="email" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="password" className="input input-bordered w-full max-w-xs" />
+                        <button className="btn btn-accent">Login</button>
                     </div>
                 </div>
             </div>
