2c378d0e63e83b95e9baa8f72e61610f4eded3ea
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-02-11 15:07:38 -0600

    update signup and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,10 +6,10 @@ const Login = () => {
             <Nav></Nav>
             <div className="grow-[1] flex justify-center items-center">
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <div className="card-body flex flex-col items-center gap-2">
+                    <div className="card-body flex flex-col items-center gap-3">
                         <h2 className="card-title">Welcome Back!</h2>
-                        <input type="text" placeholder="email" className="input input-bordered w-full max-w-xs" />
-                        <input type="text" placeholder="password" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
+                        <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs" />
                         <button className="btn btn-accent">Login</button>
                     </div>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
     themes: ["pastel"]
   }
 }
