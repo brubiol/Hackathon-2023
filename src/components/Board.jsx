9684888fc4245cf0324c4f3a4df3f57538e9f20f
Heidy Gallardo <heidy.gallardo01@utrgv.edu>
2023-02-12 06:35:17 -0600

    board_update

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,43 +1,59 @@
 import Nav from "./Nav";
-
+import { Query } from "appwrite";
 import { createAvatar } from '@dicebear/core';
 import { lorelei } from '@dicebear/collection';
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { account } from "../appwrite/appwriteConfig";
+import { databases } from "../appwrite/appwriteConfig";
 
-let userRank = [{ rank: 5, name: "Marie", likes: 45, calories: 35}, { rank: 2, name: "john", likes: 21, calories: 15 }, {rank: 3, name: "pearl", likes: 61, calories:10}];
+//let userRank = [{ rank: 5, name: "Marie", likes: 45, calories: 35 }, { rank: 2, name: "john", likes: 21, calories: 15 }, { rank: 3, name: "pearl", likes: 61, calories: 10 }];
 
 
 const Board = () => {
 
+
     const avatar = (seedValue) => {
         return createAvatar(lorelei, {
-           // size: 128,
             seed: seedValue
         }).toDataUriSync();
     }
 
-    /*const sortAsc = () => {
-        return 
-    }*/
 
-    
     const navigate = useNavigate();
     const [UserDetails, setUserDetails] = useState(null);
+    const [userRank, setUserRank] = useState([]);
+
 
     useEffect(() => {
         const getData = account.get();
-        getData.then (
+        getData.then(
             function (response) {
                 setUserDetails(response)
+
             },
             function (error) {
                 navigate("/login")
             }
         )
-    }, [])
+        // Add your submit logic here to send the data to the server
+        let promise = databases.listDocuments('63e7dab6593b65a6cef9', '63e89b5d116735a1a113', [
+                                                       
+            Query.orderDesc("time"),
+            Query.limit(3),
+        ]);
+        
     
+        promise.then(function (response) {
+            console.log(response); // Success
+            setUserRank(response.documents);
+        }, function (error) {
+            console.log(error); // Failure
+    
+        });
+    }, [])
+
+
 
   
 
@@ -48,7 +64,7 @@ const Board = () => {
                 <h1 className="normal-case text-xl font-bold">Leaderboard</h1>
             </div>
             {userRank.map((user, index) => (
-                <div className="flex justify-center">
+                <div key={index}className="flex justify-center">
 
                     <div className="stats shadow m-5">
 
@@ -56,18 +72,18 @@ const Board = () => {
                             <div className="stat-figure text-primary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             </div>
-                            <div className="stat-value text-primary" >{user.rank}</div>
+                            <div className="stat-value text-primary" >1</div>
 
                         </div>
 
                         <div className="stat">
                             <div className="stat-figure text-secondary">
 
-                                    <div className="w-16 rounded-full">
+                                <div className="w-16 rounded-full">
 
-                                        <img src={avatar(user.name)} />
+                                    <img src={avatar(user.name)} />
 
-                                    </div>
+                                </div>
 
                             </div>
 
@@ -80,22 +96,22 @@ const Board = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             </div>
                             <div className="stat-title">Total Likes</div>
-                            <div className="stat-value text-primary">{user.likes}</div>
+                            <div className="stat-value text-primary">{user.time}</div>
                         </div>
 
                         <div className="stat">
                             <div className="stat-figure text-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                             </div>
-                            <div className="stat-title">Calories Burned</div>
-                            <div className="stat-value text-secondary">{user.calories}</div>
+                            <div className="stat-title">Minutes</div>
+                            <div className="stat-value text-secondary"></div>
                         </div>
 
 
                     </div>
 
                 </div>
-            
+
             ))}
         </div>
     );
