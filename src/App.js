a50de331bc251ef59d708cdf6ce5b831ce9d5d1d
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-02-12 04:18:53 -0600

    home page done

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Login from './pages/Login';
 import Leaderboard from './pages/Leaderboard';
 import Signup from './pages/Signup';
 import Post from './components/Post'
-
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-
-        <Route path='/Leaderboard' element={<Leaderboard/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/leaderboard' element={<Leaderboard/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/post' element={<Post />} />
