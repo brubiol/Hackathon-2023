5dcef106f1dcfeb3cd39c9112827661f13ea15b5
mariotrejr <122929112+mariotrejr@users.noreply.github.com>
2023-02-12 04:13:33 -0600

    current time 4:13

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,5 +1,8 @@
 // import React, { useState, useEffect } from "react";
 
+import { account } from "../appwrite/appwriteConfig";
+import { createAvatar } from '@dicebear/core';
+import { lorelei } from '@dicebear/collection';
 // const dummyData = [
 //   {
 //     id: 1,
@@ -213,116 +216,62 @@
 
 // export default Workout;
 
-import React, { useState, useEffect } from "react";
-//import axios from "axios";
+// const Workout = (props) => {
+//   return (
+//     <div className="flex flex-col justify-center items-center mt-10">
+//       {props.loading ? (
+//         <p className="text-lg text-gray-600">Loading...</p>
+//       ) : (
+//         <ul className="w-full max-w-lg">
+//           {Object.values(props.posts.documents).map((post, index) => {
+//             return (
+//               <li
+//                 key={index}
+//                 className="bg-white shadow-md rounded p-8 my-6 border-2"
+//               >
+//                 {post.name}
+//                 {post.caption}
+//                 {post.workout}
+//                 {post.time}
+//                 {post.likes}
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
 
-const dummyData = [
-    {
-      id: 1,
-      title: "Workout 1",
-      workoutTime: "9:00 AM",
-      height: "6ft",
-      weight: "180lbs",
-      user: {
-        name: "John Doe",
-        profilePicture: "https://staticg.sportskeeda.com/editor/2023/01/3efbd-16734525751485-1920.jpg"
-      },
-      likes: 10
-    },
-    {
-      id: 2,
-      title: "Workout 2",
-      workoutTime: "11:00 AM",
-      height: "5ft 8in",
-      weight: "150lbs",
-      user: {
-        name: "Jane Doe",
-        profilePicture: "https://staticg.sportskeeda.com/editor/2023/01/3efbd-16734525751485-1920.jpg"
-      },
-      likes: 20
-    },
-    {
-      id: 3,
-      title: "Workout 3",
-      workoutTime: "2:00 PM",
-      height: "5ft 10in",
-      weight: "160lbs",
-      user: {
-        name: "Jim Smith",
-        profilePicture: "https://staticg.sportskeeda.com/editor/2023/01/3efbd-16734525751485-1920.jpg"
-      },
-      likes: 15
-    }
-  ];
-  
-
-const Workout = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-       setPosts(dummyData);
-       }, []);
-
-//   useEffect(() => {
-//     setLoading(true);
-//     axios
-//       .get("<appwrite-api-endpoint>/posts")
-//       .then(res => {
-//         setPosts(res.data.posts);
-//         setLoading(false);
-//       })
-//       .catch(err => {
-//         console.error(err);
-//         setLoading(false);
-//       });
-//   }, []);
-
-  const handleLike = id => {
-    // Implementation for handling like action
-  };
-
+// export default Workout;
+const Workout = (props) => {
   return (
+    
     <div className="flex flex-col justify-center items-center mt-10">
-      {loading ? (
+      {props.loading ? (
         <p className="text-lg text-gray-600">Loading...</p>
       ) : (
         <ul className="w-full max-w-lg">
-          {posts.map(post => (
-            <li key={post.id} className="bg-white shadow-md rounded p-8 my-6 border-2">
-              <div className="flex">
-                <img
-                  className="w-16 h-12 rounded-full mr-6"
-                  src={post.user.avatar || "https://staticg.sportskeeda.com/editor/2023/01/3efbd-16734525751485-1920.jpg"}
-                  alt={post.user.name}
-                />
-                <div>
-                  <h2 className="text-xl font-bold">{post.user.name}</h2>
-                  <p className="text-gray-600 mt-2">
-                    Workout Time: {post.workoutTime}
-                  </p>
-                  <p className="text-gray-600 mt-2">
-                    Height: {post.height}
-                  </p>
-                  <p className="text-gray-600 mt-2">
-                    Weight: {post.weight}
-                  </p>
+          {Object.values(props.posts.documents).map((post, index) => {
+            return (
+              <li
+                key={index}
+                className="bg-white shadow-md rounded p-8 my-6 border-2"
+              >
+                <div className="text-lg font-bold">{post.name}</div>
+                <div className="text-sm text-gray-600 mt-2">
+                  {post.caption}
                 </div>
-              </div>
-              <p className="text-gray-700 mt-2">{post.body}</p>
-              <div className="flex justify-between mt-4">
-                <button
-                  className="bg-blue-300 hover:bg-blue-400 text-black-800 font-bold py-2 px-4 rounded-l"
-                  onClick={() => handleLike(post.id)}
-                >
-                  Like
-                </button>
-                <p className="text-blue-600">
-                  {post.likes} likes
-                </p>
-              </div>
-            </li>
-          ))}
+                <div className="text-base mt-4">{post.workout}</div>
+                <div className="text-sm text-gray-600 mt-2">
+                  {post.time}
+                </div>
+                <div className="text-sm text-gray-600 mt-2">
+                  {post.likes} Likes
+                </div>
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
@@ -330,6 +279,3 @@ const Workout = () => {
 };
 
 export default Workout;
-
-
-
