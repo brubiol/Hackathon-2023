10be94c2194e94855edc9e6927f4c170f0a79bf3
mariotrejr <122929112+mariotrejr@users.noreply.github.com>
2023-02-12 05:39:10 -0600

    mario current 5:39

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -122,13 +122,10 @@ const PostModal = () => {
 
 
         return (
-            <div>
-                <Nav></Nav>
+          
        
-                <div className="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
-                    <div className="fixed inset-0 transition-opacity">
-                        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-                    </div>
+            
+              
                     <div className="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
                         <form onSubmit={handleSubmit}>
                         {errorMessage !== "" && (
@@ -191,8 +188,8 @@ const PostModal = () => {
                             </div>
                         </form>
                     </div>
-                </div>
-            </div>
+      
+        
         );
    
 
