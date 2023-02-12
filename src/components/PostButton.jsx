a7bb9d44ded113c5d1a22ef83adb3bd7ac587bbf
mariotrejr <122929112+mariotrejr@users.noreply.github.com>
2023-02-12 04:13:52 -0600

    current time 4:13

--- src/components/PostButton.jsx
+++ src/components/PostButton.jsx
@@ -1,34 +1,21 @@
-import React from 'react';
-import Post from './Post'
+import React from "react";
+import Post from "./Post";
 
-const PostButton = () => {
+const PostButton = (props) => {
   return (
     <div className="fixed bottom-0 right-0 m-8">
-      
-        <label htmlFor="my-modal-5" className="btn bg-blue-500 text-white">Post</label>
+      <label htmlFor="my-modal-5" className="btn bg-blue-500 text-white">
+        Post
+      </label>
 
-        <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+      <input type="checkbox" id="my-modal-5" className="modal-toggle" />
 
-
-         <div className="modal modal-bottom sm:modal-middle">
-            <div className="modal-box  ">
-
-             <div >
-            <Post/>
-            </div>
-                
-             
-             <div className="modal-action">
-                <label htmlFor="my-modal-5" className="btn">Yay!</label>
-                     </div>
-                 </div>
-            </div>
-
-
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box  ">
+          <Post UserDetails={props.UserDetails} reqPosts={props.reqPosts}/>
+        </div>
+      </div>
     </div>
-   
-
-    
   );
 };
 
